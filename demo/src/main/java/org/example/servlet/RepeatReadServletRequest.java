be803c47948b958938b34b455d5ea471fed58863
jiadianxia001 <jiadianxia001@ke.com>
2021-04-14 15:36:51 +0800

    备注

--- demo/src/main/java/org/example/servlet/RepeatReadServletRequest.java
+++ demo/src/main/java/org/example/servlet/RepeatReadServletRequest.java
@@ -1,5 +1,7 @@
 package org.example.servlet;
 
+import org.springframework.util.StreamUtils;
+
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -16,66 +18,55 @@ import java.io.InputStreamReader;
  * @date 2021/4/13 12:13
  */
 public class RepeatReadServletRequest extends HttpServletRequestWrapper {
-    private byte[] body;
-    private boolean open = false;
+    /**
+     * 用于将流保存下来
+     */
+    private final byte[] requestBody;
 
-    public RepeatReadServletRequest(HttpServletRequest request, byte[] body) {
+    public RepeatReadServletRequest(HttpServletRequest request) throws IOException {
         super(request);
-        this.body = body;
+        requestBody = StreamUtils.copyToByteArray(request.getInputStream());
     }
 
     /**
      * 返回InputStream
+     *
      * @return
-     * @throws IOException
      */
     @Override
-    public ServletInputStream getInputStream() throws IOException {
-        if (open) {
-            throw new IllegalStateException("Cannot re-open input stream!");
-        }
-        open = true;
+    public ServletInputStream getInputStream() {
+        final ByteArrayInputStream bais = new ByteArrayInputStream(requestBody);
         return new ServletInputStream() {
-            private int offset = 0;
+            @Override
+            public int read() {
+                return bais.read();
+            }
 
             @Override
             public boolean isFinished() {
-                return offset >= body.length;
+                return false;
             }
 
             @Override
             public boolean isReady() {
-                return true;
+                return false;
             }
 
             @Override
-            public void setReadListener(ReadListener listener) {
-            }
+            public void setReadListener(ReadListener readListener) {
 
-            @Override
-            public int read() throws IOException {
-                if (offset >= body.length) {
-                    return -1;
-                }
-                int n = body[offset] & 0xff;
-                offset++;
-                return n;
             }
         };
     }
 
     /**
      * 返回Reader
+     *
      * @return
-     * @throws IOException
      */
     @Override
-    public BufferedReader getReader() throws IOException {
-        if (open) {
-            throw new IllegalStateException("Cannot re-open reader!");
-        }
-        open = true;
-        //构造了一个新的ServletInputStream
-        return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(body), "UTF-8"));
+    public BufferedReader getReader() {
+        return new BufferedReader(new InputStreamReader(getInputStream()));
     }
+
 }
